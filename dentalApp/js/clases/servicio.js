6f887c01a5a85092aeaae65250e266259272b848
Esteban Demarchi <estebanjosedemarchi@gmail.com>
2021-10-30 20:17:53 -0300

    :recycle: Eliminada la función anterior sin jQuery

--- dentalApp/js/clases/servicio.js
+++ dentalApp/js/clases/servicio.js
@@ -41,92 +41,6 @@ class Servicio {
         return this.precio;
     }
 
-    // Este método va a dibujar el servicio en la Lista de servicios
-    // Lo agrega como una Card de Bootstrap 
-    dibujarEnLaList3a() {
-        // Cargo el div del index.html donde quiero comenzar a agregar los distintos servicios
-        const listaServiciosDisponibles = document.getElementById('lista-servicios-disponibles');
-
-        // Creo los distintos elementos, y asigno clases css y atributos.
-        const divColSm3 = document.createElement('div');
-        divColSm3.classList.add('col-sm-3');
-
-        const divCard = document.createElement('div');
-        divCard.classList.add('card','mt-4');
-
-        const cardImg = document.createElement('img');
-        cardImg.classList.add('card-img-top');
-        cardImg.src = this.getUrlImagen();
-
-        const divCardBody = document.createElement('div');
-        divCardBody.classList.add('card-body');
-
-        const cardTitle = document.createElement('h5');
-        cardTitle.classList.add('card-title');
-        cardTitle.innerText = this.getNombre();
-
-        const cardDescripcion = document.createElement('p');
-        cardDescripcion.classList.add('card-text');
-        cardDescripcion.innerText = this.getDescripcion();
-
-        const cardPrecio = document.createElement('p');
-        cardPrecio.classList.add('card-text');
-        cardPrecio.innerHTML = `<strong>Precio: </strong>$${this.getPrecio()}`;
-
-        const cardButtonAdd = document.createElement('button');
-        cardButtonAdd.classList.add('btn','btn-info');
-        cardButtonAdd.innerHTML='Agregar';
-        
-        const cardButtonDecrease = document.createElement('button');
-        cardButtonDecrease.classList.add('btn', 'btn-danger', 'ms-1');
-        cardButtonDecrease.setAttribute('disabled',true);
-        cardButtonDecrease.innerHTML="Quitar";
-        
-        cardButtonAdd.addEventListener('click', () => {
-            
-            if (this.cantidad===0) {
-                this.agregarAlArrayDeServiciosSeleccionados(this);
-                this.agregarCantidad();
-                cardButtonDecrease.removeAttribute('disabled');
-                divCard.classList.add('card-seleccionada');
-            } else {
-                this.existeElServicioEnElArrayDeSeleccionadosEntoncesAgregaUno(this);
-            }
-                
-            agregarListaSeleccionadosYDevolverMontosTotales();
-            
-        })
-
-        cardButtonDecrease.addEventListener('click', () => {
-            if (this.cantidad === 1) {
-                cardButtonDecrease.setAttribute("disabled",true);
-                divCard.classList.remove("card-seleccionada");
-                this.existeElServicioEnElArrayDeSeleccionadosEntoncesRestaUno(this);
-            }
-            else if (this.cantidad > 0 ) {
-                this.existeElServicioEnElArrayDeSeleccionadosEntoncesRestaUno(this);
-            } 
-            else{ 
-                cardButtonDecrease.setAttribute("disabled",true);
-                
-            }
-            agregarListaSeleccionadosYDevolverMontosTotales();
-        })
-
-        //Ahora debo comenzar a agregar los childs
-        divCardBody.appendChild(cardTitle);
-        divCardBody.appendChild(cardDescripcion);
-        divCardBody.appendChild(cardPrecio);
-        divCardBody.appendChild(cardButtonAdd);
-        divCardBody.appendChild(cardButtonDecrease);
-
-        divCard.appendChild(cardImg);
-        divCard.appendChild(divCardBody);
-
-        divColSm3.appendChild(divCard);
-
-        listaServiciosDisponibles.appendChild(divColSm3);
-    }
     agregarAlArrayDeServiciosSeleccionados(claseServicio) {
         serviciosSeleccionados.push(claseServicio);
     }
@@ -148,7 +62,7 @@ class Servicio {
     }
 
     dibujarEnLaLista() {
-        
+
         $('#lista-servicios-disponibles').append(
             `
             <div class="col-sm-3">
